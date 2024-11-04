bf8996e78eaee21b807802e67d7e10b1e4d4cf30
Yabeths <yabeths.assefa@outlook.com>
2024-11-04 11:57:52 -0800

    Delete it120/select-vehicle directory

--- it120/select-vehicle/query.sh
+++
@@ -1,6 +0,0 @@
-# Run all the queries from the command line:
-psql -f sample.sql
-psql -f distinct.sql
-psql -f group-by.sql
-psql -f new-idea.sql
-psql -f query.sql
--- it120/select-vehicle/sample.sql
+++
@@ -1,9 +0,0 @@
--- Report the number of older (pre 1990) registrations
-
-SELECT myear,
-    count(myear)
-FROM registration
-WHERE myear < 1990
-GROUP BY myear
-ORDER BY myear;
-
